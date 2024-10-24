5443705a125251bef28eb87fec722052806913b8
MTRFYMD <mtrfymd@gmail.com>
2024-10-24 16:05:22 +0900

    githubの環境変数設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // スタンドアロンモードを有効化して、必要最低限のファイルのみをデプロイ
     output: 'standalone'
+
+    // 環境変数の設定
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Azureで設定されたAPI URLを使用
+    },
 };
 
 export default nextConfig;
